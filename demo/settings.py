8f109e29ac911a2122bdb395acc1e82e73124ca2
shrionit <sptiwari46289@gmail.com>
2022-04-08 19:38:24 +0530

    minor fix

--- demo/settings.py
+++ demo/settings.py
@@ -3,7 +3,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(vwecdefb=wcs9txomty(f%!_bnpt7^brgnlsz%p84c8b41)4!'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 # Application definition
 
